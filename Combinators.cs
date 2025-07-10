eef0fcff03da18edd32f53b6448137754deba7c9
sweetpeteza <peter@playbk.io>
2025-07-10 14:53:00 +0200

    BATMAN

---
+++ Combinators.cs
@@ -0,0 +1,38 @@
+namespace FunctionalProgramming;
+
+public class Combinators
+{
+    // Combinator Lambda Expression
+
+    // I 𝜆a.a
+    public static A I<A>(A input) => input;
+    // K 𝜆ab.a
+    public static Func<B, A> K<A, B>(A input) => (_) => input;
+    // W 𝜆ab.abb
+    public static Func<A, B> W<A, B>(Func<A, A, B> f) => (x) => f(x, x);
+    // C 𝜆abc.acb
+    public static Func<B, A, C> C<A, B, C>(Func<A, B, C> f) => (x, y) => f(y, x);
+    // B 𝜆abc.a(bc)
+    public static Func<A, C> B<A, B, C>(Func<B, C> f, Func<A, B> g) => (x) => f(g(x));
+    // S 𝜆abc.ac(bc)
+    public static Func<A, C> S<A, B, C>(Func<B, B, C> f, Func<A, B> g) => (x) => f(g(x), g(x));
+    // D 𝜆abcd.ab(cd)
+    public static Func<C, A, D> D<A, B, C, D>(Func<C, B, D> f, Func<A, B> g) => (x, y) => f(x, g(y));
+    // B1 𝜆abcd.a(bcd)
+    public static Func<A, B, D> B_1<A, B, C, D>(Func<C, D> f, Func<A, B, C> g) => (x, y) => f(g(x, y));
+    // Ψ 𝜆abcd.a(bc)(bd)
+    public static Func<A, A, C> Psi<A, B, C>(Func<B, B, C> f, Func<A, B> g) => (x, y) => f(g(x), g(y));
+    // Φ 𝜆abcd.a(bd)(cd)
+    public static Func<A, D> Phi<A, B, C, D>(Func<A, B> f, Func<B, C, D> g, Func<A, C> h) => (x) => g(f(x), h(x));
+    // D2 𝜆abcde.a(bd)(ce)
+    public static Func<A, B, E> D_2<A, B, C, D, E>(Func<A, C> f, Func<C, D, E> g, Func<B, D> h) => (x, y) => g(f(x), h(y));
+    // Φ1 𝜆abcde.a(bde)(cde)
+    public static Func<A, B, E> Phi_1<A, B, C, D, E>(Func<C, D, E> f, Func<A, B, C> g, Func<A, B, D> h) => (x, y) => f(g(x, y), h(x, y));
+    // not part of the original combinators but useful
+    public static A Left<A, B>(A x, B _y) => x;
+    public static B Right<A, B>(A _x, B y) => y;
+
+    // TODO: Implement the following combinators
+    // E 𝜆abcde.ab(cde)
+    // Ê 𝜆abcdefg.a(bde)(cfg)
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using static System.String;
+using static FunctionalProgramming.Combinators;
+
+WriteLine("I: ({0}) -> {1}", 42, I(42)); // Outputs 42
+WriteLine("W: ((x, y) => x * y)({0}) -> {1}", 3, W<int, int>((x, y) => x * y)(3)); // Outputs 9
+WriteLine("C: ((x, y) => x - y)({0}, {1}) -> {2}", 5, 3, C<int, int, int>((x, y) => x - y)(5, 3)); // Outputs -2
+WriteLine("B: ((x) => x.ToUpper(), (x) => new string(x.Where(c => !\"aeiouAEIOU\".Contains(c)).ToArray()))({0}) -> {1}", "hello world", B<string, string, string>((x) => x.ToUpper(), (x) => new string(x.Where(c => !"aeiouAEIOU".Contains(c)).ToArray()))("hello world")); // Outputs "HLL WRLD"
+WriteLine("S: ((x, y) => x + y, (x) => x * 2)({0}) -> {1}", 3, S<int, int, int>((x, y) => x + y, (x) => x * 2)(3)); // Outputs 12
+WriteLine("D: ((x, y) => x + y, (x) => x * 2)({0}, {1}) -> {2}", 3, 4, D<int, int, int, int>((x, y) => x + y, (x) => x * 2)(3, 4)); // Outputs 11
+WriteLine("B_1: ((x) => x * 2, (x, y) => x + y)({0}, {1}) -> {2}", 3, 4, B_1<int, int, int, int>((x) => x * 2, (x, y) => x + y)(3, 4)); // Outputs 14
+WriteLine("Psi: ((x, y) => x + y, (x) => x * 2)({0}, {1}) -> {2}", 4, 5, Psi<int, int, int>((x, y) => x + y, (x) => x * 2)(4, 5)); // Outputs 18
+WriteLine("Phi: ((x) => x * 2, (x, y) => x + y, (x) => x + 1)({0}) -> {1}", 3, Phi<int, int, int, int>((x) => x * 2, (x, y) => x + y, (x) => x + 1)(3)); // Outputs 10
+WriteLine("D_2: ((x) => x * 2, (x, y) => x + y, (x) => x + 1)({0}, {1}) -> {2}", 3, 4, D_2<int, int, int, int, int>((x) => x * 2, (x, y) => x + y, (x) => x + 1)(3, 4)); // Outputs 11
+WriteLine("Phi_1: ((x, y) => x + y, (x, y) => (x + y) * 2, (x, y) => x - y)({0}, {1}) -> {2}", 3, 4, Phi_1<int, int, int, int, int>((x, y) => x + y, (x, y) => (x + y) * 2, (x, y) => x - y)(3, 4)); // Outputs 13
+WriteLine("Left: ({0}, {1}) -> {2}", 42, "hello", Left(42, "hello")); // Outputs 42
+WriteLine("Right: ({0}, {1}) -> {2}", 42, "hello", Right(42, "hello")); // Outputs 100
